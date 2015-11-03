2270a98e54a461ff6914c661596d90df335297f1
Alfred <alfzi942@student.liu.se>
2015-11-03 16:05:54 +0100

    Finished abstract Piece class

--- Chess/src/Piece.cs
+++ Chess/src/Piece.cs
@@ -10,21 +10,43 @@ namespace Chess.src
         // fields
         private uint xCoord;
         private uint yCoord;
-        private uint score;
+        private uint score;         //Value of the piece
+        private bool hasMoved;      //Determines if the piece has moved from initial pos
+        private string colour;      //White or black
+
 
         // methods
-        void move(uint x, uint y)
+        public void move(uint x, uint y)
         {
-            if (movePossible(x,y))
-            {
-                this.xCoord = x;
-                this.yCoord = y;
-            }
+            this.xCoord = x;
+            this.yCoord = y;
+            if (hasMoved)
+                hasMoved = false;
         }
 
-        bool movePossible(uint x, uint y)
-        {
+        //returns a list of all possible moves
+        public abstract List<Tuple<uint, uint>> getPossibleMoves(Board.QueryFunc QF);
 
+        //Checks if move is possible
+        public bool movePossible(uint x, uint y, Board.QueryFunc QF)
+        {
+            List<Tuple<uint, uint>> tmp;
+            tmp = this.getPossibleMoves(QF);
+            foreach (Tuple<uint, uint> item in tmp)
+            {
+                if ((item.Item1 == x) && (item.Item2 == 2))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Used to determine if the piece has moved from initial position.
+        // Nessecary for En Passant and Castling
+        bool movedFromInit()
+        {
+            return hasMoved;
         }
     }
 }
