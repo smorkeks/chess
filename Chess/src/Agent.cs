23de950f897022ed4152d352a8a74e637c8e48a8
Alfred <alfzi942@student.liu.se>
2015-11-03 18:11:17 +0100

    Added abstract Agent class

---
+++ Chess/src/Agent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chess.src
+{
+    public class Agent
+    {
+        // Fields
+
+
+
+        // Methods
+        public abstract Tuple<uint, uint, uint, uint> getInput(Board B);
+    }
+}
