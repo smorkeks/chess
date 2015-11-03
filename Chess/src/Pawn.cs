19fc4b6fb2467aacfcf45d9f29053e07c4ef3268
Alfred <alfzi942@student.liu.se>
2015-11-03 17:23:45 +0100

    Pawn finished

--- Chess/src/Pawn.cs
+++ Chess/src/Pawn.cs
@@ -7,9 +7,12 @@ namespace Chess.src
 {
     public class Pawn : Piece
     {
+
+        public Pawn(uint x, uint y, string col) : base(x, y, 1, col) { }
+
         public override List<Tuple<uint, uint>> getPossibleMoves(Board.QueryFunc QF)
         {
-            List<Tuple<uint, uint>> tmpList = new List<Tuple<uint,uint>>();
+            List<Tuple<uint, uint>> tmpList = new List<Tuple<uint, uint>>();
             short yMod;
 
             if (this.colour == "white")
@@ -17,21 +20,54 @@ namespace Chess.src
             else
                 yMod = -1;
 
-            if (withinBoard((int) getX() - 1, (int) getY() + yMod))
+            if (withinBoard((int)getX() - 1, (int)getY() + yMod))
             {
-                Square S = QF((uint) (getX() - 1), (uint) (getY() + yMod));
+                Square S = QF((uint)(getX() - 1), (uint)(getY() + yMod));
                 Piece P = S.getPiece();
                 if (P != null)
                 {
                     if (!isSameColour(P))
                     {
-                        tmpList.Add(new Tuple<uint, uint>((uint)(getX() - 1),(uint)(getY() + yMod)));
+                        tmpList.Add(new Tuple<uint, uint>((uint)(getX() - 1), (uint)(getY() + yMod)));
                     }
                 }
             }
 
-            
+            if (withinBoard((int)getX() + 1, (int)getY() + yMod))
+            {
+                Square S = QF((uint)(getX() + 1), (uint)(getY() + yMod));
+                Piece P = S.getPiece();
+                if (P != null)
+                {
+                    if (!isSameColour(P))
+                    {
+                        tmpList.Add(new Tuple<uint, uint>((uint)(getX() + 1), (uint)(getY() + yMod)));
+                    }
+                }
+            }
 
+            if (withinBoard((int)getX(), (int)getY() + yMod))
+            {
+                Square S = QF(getX(), (uint)(getY() + yMod));
+                Piece P = S.getPiece();
+                if (P == null)
+                {
+                    tmpList.Add(new Tuple<uint, uint>(getX(), (uint)(getY() + yMod)));
+                }
+            }
+
+            if (!movedFromInit())
+            {
+                if (withinBoard((int)getX(), (int)getY() + 2 * yMod))
+                {
+                    Square S = QF(getX(), (uint)(getY() + 2 * yMod));
+                    Piece P = S.getPiece();
+                    if (P == null)
+                    {
+                        tmpList.Add(new Tuple<uint, uint>(getX(), (uint)(getY() + 2 * yMod)));
+                    }
+                }
+            }
             return tmpList;
         }
     }
